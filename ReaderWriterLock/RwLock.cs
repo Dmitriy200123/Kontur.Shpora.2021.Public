1bb92a197a5de67c26723063a62f8f0b2ec05a83
Dima <d.d.demin@urfu.me>
2021-04-20 18:48:25 +0500

    Fixed implementation

--- ReaderWriterLock/RwLock.cs
+++ ReaderWriterLock/RwLock.cs
@@ -5,22 +5,20 @@ namespace ReaderWriterLock
 {
     public class RwLock : IRwLock
     {
-        private int _writersCount;
         private int _readersCount;
         private readonly object _lockObj = new();
         public void ReadLocked(Action action)
         {
             lock (_lockObj)
             {
-                while (_writersCount > 0)
-                    Monitor.Wait(_lockObj);
                 _readersCount++;
             }
             action.Invoke();
             lock (_lockObj)
             {
                 _readersCount--;
-                Monitor.Pulse(_lockObj);
+                if (_readersCount == 0)
+                    Monitor.PulseAll(_lockObj);
             }
         }
 
@@ -28,12 +26,9 @@ namespace ReaderWriterLock
         {
             lock (_lockObj)
             {
-                while (_writersCount > 0 || _readersCount > 0)
+                while (_readersCount > 0)
                     Monitor.Wait(_lockObj);
-                _writersCount++;
                 action.Invoke();
-                _writersCount--;
-                Monitor.PulseAll(_lockObj);
             }
         }
     }
